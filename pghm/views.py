10a8f7ea38ead0c017b0e86595b587cf6bd729c8
zer0mode <c.oton@protonmail.com>
2016-09-09 14:11:54 +0200

    some cleaning

--- pghm/views.py
+++ pghm/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from djgeojson.views import GeoJSONLayerView
 from django.http import HttpResponse, HttpResponseBadRequest
-from .models import Webcam
-from .models import Unite
-# from django.core.serializers import serialize
+# from .models import Webcam
+# from .models import Unite
+from django.core.serializers import serialize
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    #return HttpResponse("This is hopefully the index of colorado rocks view.")
     return render(request, 'index.html')
 
 class MapLayer(GeoJSONLayerView):
@@ -16,13 +15,10 @@ class MapLayer(GeoJSONLayerView):
     precision = 4   # float
     simplify = 0.5  # generalization
 
-
-
 # def wcamdata(request):
 #     #return HttpResponse("This is hopefully the index of colorado rocks view.")
 #     return render(request, 'pghm/data/webcam2.geojson')
 
-#def cam_view(request):
+# def cam_view(request):
 #    cams_as_geojson = serialize('geojson', Webcam.objects.all())
 #    return HttpResponse(cams_as_geojson, content_type='json')
-
